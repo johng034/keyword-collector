c58312ea358412411a561f58d8794ccf2581cf34
johng034 <67410822+johng034@users.noreply.github.com>
2022-08-23 13:31:11 -0700

    create a similarity score to compare resume with the job description

--- app.py
+++ app.py
@@ -11,10 +11,11 @@ st.set_page_config(page_title='Keyword Finder')
 def clean_text(text):
     text = re.sub('\n', ' ', text)  # Remove '\n'
     text = text.strip()  # Remove whitespaces
+    text = text.lower()  # Lowercase all the text
     text = re.sub("'", '', text)  # Remove ' with no spaces (e.g., they're -> theyre)
     text = re.sub(",", '', text)  # Remove , with no spaces (e.g., 10,000 -> 10000)
+    text = re.sub("etc", '', text)  # Remove , with no spaces (e.g., 10,000 -> 10000)
     text = re.sub('[^A-Za-z0-9]+', ' ', text)  # Remove special characters
-    text = text.lower()  # Lowercase all the text
     text = text.strip()  # Remove any final whitespaces
     return text
 
@@ -31,6 +32,8 @@ st.markdown("""
             Please feel free to check out the [Github repository](https://github.com/johng034/keyword-collector).
             """)
 
+## ----------------------------------------------------------------------------------------------
+# JOB DESCRIPTION
 job_description = st.text_input(label='Copy and paste the job description below:')
 
 if job_description != '':
@@ -41,25 +44,36 @@ if job_description != '':
 
     X = vectorizer.fit_transform(word_list)
 
-    # Create a DataFrame 
-    df = pd.DataFrame(data=X.toarray(), columns=vectorizer.get_feature_names_out())
+    words = vectorizer.get_feature_names_out()
+    frequency = X.toarray().sum(axis=0)
 
-    # Sort the values
-    words = df.sum(axis=0).sort_values(ascending=False)
+    data = {
+            'Keyword': words,
+            'Frequency': frequency
+            }
+
+    # Create a DataFrame 
+    df = pd.DataFrame(data=data)
+
+    # Sort the keywords by frequency and save the top 25%
+    words = df.sort_values(by='Frequency', ascending=False).reset_index(drop=True)
+    twenty_five_percent = int(len(words)*0.25)
+    top_keywords = words[:twenty_five_percent]
 
     # Create button to see keywords
     if st.button(label='View Keywords'):
-        st.dataframe(words[:25])
+        st.dataframe(top_keywords)
 
-
-
-
+## ----------------------------------------------------------------------------------------------
+# RESUME
 # Upload a file (PDF)
 st.subheader('Compare your resume to the job description.')
 uploaded_file = st.file_uploader(label='Upload your resume', type='pdf')
 
+## ----------------------------------------------------------------------------------------------
+# RESULTS
 if st.button(label='Compare'):
-    if uploaded_file != None:
+    if uploaded_file != None and job_description != '':
         with pdfplumber.open(uploaded_file) as pdf:
 
             # Go through each page and extract text
@@ -68,13 +82,34 @@ if st.button(label='Compare'):
                 pdf_content = page.extract_text()
                 complete_resume.append(pdf_content)
 
-        # Save complete text
+        # Collect and clean the text from the resume
         resume_text = '\n'.join(complete_resume)
         cleaned_text = clean_text(resume_text)
-        st.markdown(cleaned_text.strip())
 
-        ## TODO Count the number of instances a keyword is in the resume
-        ## TODO Weigh the most frequent keywords more
+        # Calculate the similarity between the resume and job description
+        n_keywords = 0
+        included_words = []
+        missing_words = []
+
+        # Iterate through top job description keywords
+        for word in top_keywords['Keyword']:
+            # Check if keyword is in the resume 
+            if word in cleaned_text.split():
+                n_keywords += 1
+                included_words.append(word)
+            # If the keyword is not in the resume
+            else:
+                missing_words.append(word)
+
+        st.subheader('Results')
+
+        percentage = (n_keywords/twenty_five_percent)*100
+        st.subheader(f'{round(percentage, 1)}%')
+        st.markdown(f'Your resume contains {n_keywords} of the top {twenty_five_percent} keywords found in the job description.')
+        st.markdown(f'The keywords **included** in your resume are: {", ".join([word.capitalize() for word in included_words])}')
+        st.markdown(f'The following words are *missing* from your resume: {", ".join([word.capitalize() for word in missing_words])}')
+
+        ## TODO Place a higher weight on the most frequent keywords 
 
     else:
-        st.text('Please upload a file and try again.')
+        st.text('Please add a job description and upload your resume and try again.')
